7a85abe539c7804845b12a4d570155e53c0fe12c
igor <souzaamorimdasilvai@gmail.com>
2023-12-11 20:46:10 -0300

    Refatory Profile

---
+++ src/pages/ProfileEdit/style.js
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+
+
+        export const Container = styled.div`
+
+        width: 100%;
+        min-height: 100vh;
+
+        padding: 10px 64px 0;
+
+        background: ${({ theme }) => theme.COLORS.BACKGROUND_800} fixed;
+
+        overflow-y: auto;
+
+`;
